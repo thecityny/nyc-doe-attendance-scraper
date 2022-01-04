219755cea4d074832a7a0c463bc4b1760c4d6ce6
Will Welch <wwelch@thecity.nyc>
2022-01-04 17:20:31 -0500

    Added CSV scraping

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const axios = require("axios");
 const FormData = require('form-data');
+const {stringify} = require('csv-stringify/sync');
 
 const jsdom = require("jsdom");
 const virtualConsole = new jsdom.VirtualConsole();
@@ -11,9 +12,12 @@ async function downloadData(path) {
   // Request page to get form data
   const pageRequest = await axios.get("https://www.nycenet.edu/PublicApps/Attendance.aspx");
   const pageDOM = new JSDOM(pageRequest.data, {virtualConsole});
-  const formInputs = Array.from(pageDOM.window.document.querySelectorAll("#form1 input"));
+
+  // Scrape overall pct
+  const overallPct = pageDOM.window.document.querySelector("#ContentPlaceHolder1_lblPcnt").innerHTML.slice(0, -1);
 
   // Compile form data
+  const formInputs = Array.from(pageDOM.window.document.querySelectorAll("#form1 input"));
   const form = new FormData();
   form.append("__EVENTTARGET", "ctl00$ContentPlaceHolder1$btnExport");
   formInputs.forEach(input => {
@@ -41,9 +45,36 @@ async function downloadData(path) {
     return Object.fromEntries(rowEntries);
   });
 
+  // Add overall pct to the data
+  const attendanceDate = rowData[0]["ATTD DATE"];
+  const data = [{
+    "SCHOOL": "ALL",
+    "SCHOOL NAME": "ALL",
+    "ATTD DATE": attendanceDate,
+    "%ATTD": overallPct
+  }, ...rowData];
+
+  const csvData = stringify(data, {
+    header: true
+  });
+
+  // Create the data directory if it doesn't exist
+  const dataDir = "data";
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
+  }
+
+  const dateMatch = attendanceDate.match(/^(\d+)\/(\d+)\/(\d+)\s/);
+  const dateParts = dateMatch && dateMatch.slice(1).map(d => d.padStart(2, "0"));
+  const csvPath = `${dataDir}/${dateParts[2]}-${dateParts[0]}-${dateParts[1]}.csv`;
+
+  // Write data as CSV
+  console.log(`Saving CSV to ${csvPath}`);
+  fs.writeFileSync(csvPath, csvData);
+
   // Write data as JSON
-  console.log(`Saving data to ${path}`);
-  fs.writeFileSync(path, JSON.stringify(rowData, null, 2));
+  console.log(`Saving JSON to ${path}`);
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
 };
 
 async function main() {
